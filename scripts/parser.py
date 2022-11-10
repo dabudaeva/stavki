db6fec23fa0d9ca077240310de880bb1463d7581
Petrucho <petrucho@post.cz>
2022-11-10 21:25:13 +0300

    integrating files to project

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,10 +1,10 @@
-from parsing.BaltBet_WC2022_short import baltbet
+# from parsing.BaltBet_WC2022_short import baltbet
 from parsing.BetCity_WC2022_short import betcity
-from parsing.FonBet_WC2022_short import fonbet
-from parsing.LeonBet_WC2022_short import leonbet
-from parsing.OlimpBet_WC2022_short import olimpbet
-from parsing.Pari_WC2022_short import pari
-from parsing.SportBet_WC2022_short import sportbet
-from parsing.ZenitBet_WC2022_short import zenitbet
+# from parsing.FonBet_WC2022_short import fonbet
+# from parsing.LeonBet_WC2022_short import leonbet
+# from parsing.OlimpBet_WC2022_short import olimpbet
+# from parsing.Pari_WC2022_short import pari
+# from parsing.SportBet_WC2022_short import sportbet
+# from parsing.ZenitBet_WC2022_short import zenitbet
 
-baltbet()
+betcity()
--- scripts/parsing/BetCity_WC2022_short.py
+++ scripts/parsing/BetCity_WC2022_short.py
@@ -10,8 +10,11 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 from fake_useragent import UserAgent
 
-import split_div_BetCity
-import save_to_csv
+# import split_div_BetCity
+# import save_to_csv
+from parsing.split_div_BetCity import split_list
+from parsing.save_to_csv import save_to_file
+
 def betcity():
     user_agent = UserAgent(verify_ssl=False).chrome
     # print(f"\nuser_agent: {user_agent}\n")
@@ -40,15 +43,17 @@ def betcity():
         # print(f'page_source: {page_source}')
 
         # save screenshot of the page
-        driver.save_screenshot('../../data/BetCity_world_cup_2022.png')
+        driver.save_screenshot('./data/BetCity_world_cup_2022.png')
 
         xp_block = '//div[@class="line__champ"]'   
         block_name = driver.find_elements(By.XPATH, xp_block)    
         block_name_list = [value.text for value in block_name]
         # print(f'\nblock_name_list: {block_name_list}\n')
 
-        splitted_list = split_div_BetCity.split_list(block_name_list) # split List
-        save_to_csv.save_to_file(splitted_list) # splitted List saving to csv-file
+        # splitted_list = split_div_BetCity.split_list(block_name_list) # split List
+        splitted_list = split_list(block_name_list) # split List
+        # save_to_csv.save_to_file(splitted_list) # splitted List saving to csv-file
+        save_to_file(splitted_list) # splitted List saving to csv-file
         
     except:
         print("some error happen !!")
