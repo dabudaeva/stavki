61c1370cb6bc7d723fcfd6555a8dcb1bd5099e55
vlbudaeva <vl.budaeva@gmail.com>
2022-11-14 06:39:14 +0100

    modified

--- app.py
+++ app.py
@@ -156,17 +156,6 @@ def fork():
     )
 
 
-@app.route("/datapage", methods=['GET', 'POST'])
-def data_page():
-    if request.method == 'POST':
-        filesDict = request.files.to_dict()
-        uploadData = request.files['media']
-        data_file_name = uploadData.filename
-        uploadData.save(os.path.join(app.root_path, 'uploads\\' + data_file_name))
-
-    return render_template("upload.html")
-
-
 
 
 if __name__ == '__main__':
